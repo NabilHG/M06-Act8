e292af39a42526af9f376cb173f155e076d5ae7b
NabilHG <nabilhajjoune@gmail.com>
2024-03-24 21:21:25 +0100

    finished activity

--- index.js
+++ index.js
@@ -1,56 +1,55 @@
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     getData(1)
-    .then(function(data) {
-        console.log(data);
-        printData(data);
-    })
-    .catch(function(error) {
-        console.error(error);
-    });
+        .then(function (data) {
+            printData(data);
+        })
+        .catch(function (error) {
+            console.error(error);
+        });
 });
 
 let prevBtn = document.getElementById("prevBtn");
 let nextBtn = document.getElementById("nextBtn");
 
-prevBtn.addEventListener('click', function(){
-    console.log("as");
+prevBtn.addEventListener('click', function () {
     let num = (parseInt(nextBtn.getAttribute('data-page')) - 1);
     num = num == 0 ? 1 : num;
     prevBtn.setAttribute('data-page', num);
     nextBtn.setAttribute('data-page', num);
     getData(num)
-    .then(function(data) {
-        console.log(data);
-        printData(data);
-    })
-    .catch(function(error) {
-        console.error(error);
-    });
+        .then(function (data) {
+            printData(data);
+        })
+        .catch(function (error) {
+            console.error(error);
+        });
 });
 
-nextBtn.addEventListener('click', function(){
-    console.log("sa");
+nextBtn.addEventListener('click', function () {
+
     let num = parseInt(nextBtn.getAttribute('data-page')) + 1;
     nextBtn.setAttribute('data-page', num);
     prevBtn.setAttribute('data-page', num);
     getData(num)
-    .then(function(data) {
-        console.log(data);
-        printData(data);
-    })
-    .catch(function(error) {
-        console.error(error);
-    });
+        .then(function (data) {
+            printData(data);
+        })
+        .catch(function (error) {
+            console.error(error);
+        });
 });
 
 
-function getData(num) {
-    return new Promise(function(resolve, reject) {
+
+
+function getData(num=null) {
+    return new Promise(function (resolve, reject) {
         let xmlHttp = new XMLHttpRequest();
-        let urlDestino = "https://swapi.py4e.com/api/people/?page="+num;
+        let urlDestino = "https://swapi.py4e.com/api/people/?page=" + num;
+
         xmlHttp.open("GET", urlDestino, true);
-        xmlHttp.onreadystatechange = function() {
+        xmlHttp.onreadystatechange = function () {
             if (xmlHttp.readyState == 4) {
                 if (xmlHttp.status == 200) {
                     resolve(xmlHttp.responseText);
@@ -63,45 +62,13 @@ function getData(num) {
     });
 }
 
-// getData()
-//     .then(function(data) {
-//         console.log(data);
-//         printData(data);
-//     })
-//     .catch(function(error) {
-//         console.error(error);
-//     });
-
-
-// function printData(data){
-//     const jsonData = JSON.parse(data);
-//     const people = jsonData.results;
-//     let infoDiv = document.getElementById("info");
-    
-//     infoDiv.innerHTML = ""; // Limpiamos el contenido previo del div
-
-//     people.forEach(person => {
-//         let personInfo = document.createElement("div");
-//         personInfo.innerHTML = `
-//             <p>Name: ${person.name}</p>
-//             <p>Height: ${person.height}</p>
-//             <p>Hair: ${person.hair_color}</p>
-//             <p>Skin: ${person.skin_color}</p>
-//             <p>Eyes: ${person.eye_color}</p>
-//             <p>Birth Year: ${person.birth_year}</p>
-//             <p>Gender: ${person.gender}</p>
-//             <hr>
-//         `;
-//         infoDiv.appendChild(personInfo);
-//     });
-// }
 
 function printData(data) {
     const jsonData = JSON.parse(data);
     const people = jsonData.results;
     let infoDiv = document.getElementById("info");
 
-    infoDiv.innerHTML = ""; // Limpiamos el contenido previo del div
+    infoDiv.innerHTML = "";
 
     people.forEach(person => {
         let personInfo = document.createElement("div");
@@ -113,48 +80,264 @@ function printData(data) {
             <p>Eyes: ${person.eye_color}</p>
             <p>Birth Year: ${person.birth_year}</p>
             <p>Gender: ${person.gender}</p>
-            <input type="file" class="fileInput">
-            <button class="uploadButton">Upload</button>
             <hr>
         `;
         infoDiv.appendChild(personInfo);
 
-        // Agregar listener para cada botón de carga
-        let uploadButtons = personInfo.querySelectorAll(".uploadButton");
-        uploadButtons.forEach(button => {
-            button.addEventListener("click", function() {
-                let fileInput = this.previousSibling; // Obtener el input de archivo asociado
-                let file = fileInput.files[0]; // ERROR:  Cannot read properties of undefined (reading '0') 
+        let inputFile = document.createElement("input");
+        let uploadButton = document.createElement("button");
+        uploadButton.textContent = "Upload"
+        inputFile.type = "file";
+        personInfo.appendChild(inputFile);
+        personInfo.appendChild(uploadButton);
+        const characterInfo = {
+            name: person.name,
+            height: person.height,
+            hair_color: person.hair_color,
+            skin_color: person.skin_color,
+            eye_color: person.eye_color,
+            birth_year: person.birth_year,
+            gender: person.gender,
+        }
+        uploadButton.addEventListener("click", (e) => {
+            e.preventDefault()
 
-                if (file) {
-                    let formData = new FormData();
-                    formData.append("photo", file);
-                    // Agregar la información de la persona al FormData
-                    formData.append("name", person.name);
-                    formData.append("height", person.height);
-                    formData.append("hair_color", person.hair_color);
-                    formData.append("skin_color", person.skin_color);
-                    formData.append("eye_color", person.eye_color);
-                    formData.append("birth_year", person.birth_year);
-                    formData.append("gender", person.gender);
+            let form = new FormData();
+            form.append('characterInfo', JSON.stringify(characterInfo))
+            form.append("file", inputFile.files[0]);
 
-                    fetch("/persona/", {
-                        method: "POST",
-                        body: formData
-                    })
-                    .then(response => response.json())
-                    .then(data => {
-                        console.log("File uploaded successfully:", data);
-                        // Aquí puedes manejar la respuesta del servidor si es necesario
-                    })
+            fetch("http://localhost:8001/persona", {
+                method: "POST",
+                body: form
+            })
+                .then(
+                    (resp) => {
+                        resp.json().then(
+                            (respJSON) => {
+                            }
+                        )
+                    }
+                )
+        })
+        personInfo.addEventListener('click', (e) => {
+            const fileName = characterInfo.name.replace(/ /g, '_');
+            if (e.target.tagName !== "BUTTON" && e.target.tagName !== "INPUT") {
+                fetch("http://localhost:8001/persona/" + fileName, {
+                    method: "GET",
+                }).then((resp) => {
+                    resp.json().then(
+                        (respJSON) => {
+
+                            const overlay = document.createElement('div');
+                            overlay.setAttribute("style", "position: fixed; top: 0; left: 0; width: 100%; height: 100%; background-color: rgba(0,0,0,0.5); z-index: 999;");
+
+                            const div = document.createElement('div');
+                            div.setAttribute("style", "position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%); background-color: white; padding: 20px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); z-index: 1000;");
+                            div.innerHTML = `
+                                <h2>${respJSON.name} Info</h2>
+                                <p><strong>Name:</strong> ${respJSON.name}</p>
+                                <p><strong>Height:</strong> ${respJSON.height} cm</p>
+                                <p><strong>Hair:</strong> ${respJSON.hair_color}</p>
+                                <p><strong>Skin:</strong> ${respJSON.skin_color}</p>
+                                <p><strong>Eyes:</strong> ${respJSON.eye_color}</p>
+                                <p><strong>Birth Year:</strong> ${respJSON.birth_year}</p>
+                                <p><strong>Genre:</strong> ${respJSON.gender}</p>
+                                ${respJSON.img ? `<img src="./server/data/${respJSON.img}" alt="Img ${respJSON.name}" style="width:70%; height: 300px">` : ''}
+                            `;
+
+                            overlay.appendChild(div);
+
+                            document.body.appendChild(overlay);
+
+                            overlay.addEventListener('click', (event) => {
+                                if (event.target === overlay) {
+                                    document.body.removeChild(overlay);
+                                }
+                            });
+
+                            div.addEventListener('click', (event) => {
+                                event.stopPropagation();
+                            });
+                        }
+                    )
+                })
                     .catch(error => {
-                        console.error("Error uploading file:", error);
                     });
-                } else {
-                    console.error("No file selected.");
-                }
-            });
+
+            }
         });
     });
 }
 
+
+document.getElementById("startBtn").addEventListener("click", () => {
+    document.getElementById("optionsCharacters").innerHTML = "";
+    document.getElementById("comparationCharacters").innerHTML = "";
+    document.getElementById("starwadleInput").value = "";
+    document.getElementById("starwadleInput").style.display = "block";
+    document.getElementById("checkBtn").style.display = "block";
+    let wordle = document.getElementById("interactWordle");
+    wordle.classList.remove('d-none');
+    wordle.classList.add('d-flex');
+    let guess = document.getElementById("characterToGuessText");
+    guess.classList.remove('d-none');
+    guess.classList.add('d-block');
+
+    let start = true
+    fetch("http://localhost:8001/starwardle", {
+        method: "POST",
+        body: start
+    })
+        .then(
+            (resp) => {
+                resp.json().then(
+                    (respJSON) => {
+                        document.getElementById("characterToGuess").innerHTML = "";
+                        document.getElementById("characterToGuess").style.border = "1px solid black";
+                        document.getElementById("characterToGuess").style.width = "40%";
+                        document.getElementById("characterToGuess").style.marginTop = "10px";
+
+                        let i = 0;
+                        const keys = Object.keys(respJSON.personajeAdivinar);
+                        const intervalId = setInterval(() => {
+                            if (i < keys.length) {
+                                const atributo = keys[i];
+                                if (atributo != "name" && atributo != "img") {
+                                    const p = document.createElement("p");
+                                    p.textContent = `${atributo}: ${respJSON.personajeAdivinar[atributo]}`;
+                                    document.getElementById("characterToGuess").appendChild(p); 
+                                }
+                                i++;
+
+                            } else {
+                                clearInterval(intervalId); 
+                            }
+                        }, 50);
+                    }
+                )
+            }
+        )
+})
+
+document.getElementById("checkBtn").addEventListener("click", () => {
+    const name = document.getElementById("starwadleInput").value;
+
+    fetch("http://localhost:8001/compararPersonaje", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ nombre: name })
+    })
+        .then(resp => {
+            resp.json().then(respJSON => {
+                let div = document.createElement("div");
+                div.style.width = "40%";
+                div.style.display = "inline-block";
+                div.style.marginTop = "10px";
+                div.style.marginLeft = "5px";
+                document.getElementById("comparationCharacters").prepend(div);
+
+                let i = 0;
+                let adivinado = true;
+                const keys = Object.keys(respJSON.datos);
+                const intervalId = setInterval(() => {
+                    if (i < keys.length && keys[i] != "img") {
+                        const atributo = keys[i];
+                        const p = document.createElement("p");
+                        const valor = respJSON.datos[atributo];
+                        if (adivinado && !respJSON.comparaciones[atributo]) {
+                            adivinado = false;
+                        }
+                        const comparacion = respJSON.comparaciones[atributo];
+
+                        p.textContent = `${atributo}: ${valor}`;
+                        p.style.color = comparacion ? "green" : "red";
+
+                        if (comparacion) {
+                            p.classList.add("green");
+                        } else {
+                            p.classList.add("red");
+                        }
+
+                        div.appendChild(p);
+                        i++;
+                    } else {
+                        if (adivinado) {
+                            document.getElementById("starwadleInput").style.display = "none";
+                            let textWin = document.getElementById("youWin");
+                            textWin.classList.remove('d-none');
+                            textWin.classList.add('d-block');
+                            if(respJSON.datos["img"]){
+                                let winnerImg = document.getElementById("winnerImg");
+                                winnerImg.style.display = "block";
+                                winnerImg.style.width = "20%";
+                                winnerImg.style.height = "300px";
+                                winnerImg.setAttribute("src", "./server/data/" + respJSON.datos["img"]);
+
+                            } else {
+                                let noImgText = document.getElementById("noImgText");
+                                noImgText.innerHTML = "<b>No image found</b>";
+                                noImgText.style.display = "block";
+                            }
+                            div.style.border = "1px solid green";
+                        } else {
+                            div.style.border = "1px solid red";
+                        }
+                        clearInterval(intervalId); 
+                    }
+                }, 50); 
+
+                document.getElementById("starwadleInput").value = "";
+                document.getElementById("optionsCharacters").innerHTML = "";
+            });
+        });
+});
+
+
+
+
+document.getElementById("starwadleInput").addEventListener("keyup", (e) => {
+    if(document.getElementById("starwadleInput").value == "") {
+        document.getElementById("checkBtn").setAttribute("disabled", true);
+    }    let name = document.getElementById("starwadleInput").value;
+    fetch("http://localhost:8001/buscarPersonajes", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json', 
+        },
+        body: JSON.stringify({ name: name }) 
+    })
+        .then(resp => resp.json())
+        .then(respJSON => {
+            document.getElementById("optionsCharacters").innerHTML = "";
+            for (let i = 0; i < respJSON.length; i++) {
+                let div = document.createElement("div")
+                div.classList.add("characterOption");
+
+                div.addEventListener("click", () => {
+                    document.getElementById("checkBtn").removeAttribute("disabled")
+
+                    document.getElementById("starwadleInput").value = respJSON[i].name;
+                    document.getElementById("optionsCharacters").innerHTML = "";
+
+                })
+                if (respJSON[i].img) {
+                    let img = document.createElement("img");
+                    img.src = `./server/data/${respJSON[i].img}`;
+                    img.alt = `Imagen de ${respJSON[i].name}`;
+                    img.style.maxWidth = "100px"; 
+                    div.appendChild(img);
+                }
+                let p = document.createElement("p");
+                p.textContent = respJSON[i].name;
+                div.append(p)
+
+                document.getElementById("optionsCharacters").append(div);
+                document.getElementById("optionsCharacters").style.border = "1px solid black";
+            }
+          
+        })
+        .catch(error => {
+        });
+});
--- server/app.js
+++ server/app.js
@@ -17,19 +17,18 @@ app.post('/persona', (req, res) => {
         res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
         let imageName = null;
-        if (files['arxiu']) {
-            const arxiu = files["arxiu"][0];
-            const originalFilename = arxiu.originalFilename;
+        if (files['file']) {
+            const file = files["file"][0];
+            const originalFilename = file.originalFilename;
             const extension = originalFilename.match(/\.[^.]+$/);
 
             form.uploadDir = __dirname + '/data';
             let charactertoSave = JSON.parse(fields.characterInfo);
             const fileName = charactertoSave.name.replace(/ /g, '_');
             imageName = fileName + extension[0];
-            fs.rename(arxiu.filepath, form.uploadDir + '/' + fileName + extension[0], async (err) => {
+            fs.rename(file.filepath, form.uploadDir + '/' + fileName + extension[0], async (err) => {
                 if (err) {
                     res.statusCode = 400;
-                    // res.end(JSON.stringify({ error: "no al guardar la imatge" })); return;
                 }
             });
         }
@@ -48,7 +47,6 @@ app.post('/persona', (req, res) => {
                         if (err) {
                         } else {
                             let json_llegit = JSON.parse(data);
-                            // console.log(json_llegit)
                         }
                     });
                 }
@@ -61,7 +59,6 @@ app.get('/persona/:personaje', (req, res) => {
     let personaje = req.params.personaje;
     fs.readFile(__dirname + '/persona/' + personaje + '.json', async (err, data) => {
         if (err) {
-            console.log("error al llegir l'arxiu");
         } else {
             let json_llegit = JSON.parse(data);
             res.statusCode = 200;
@@ -72,7 +69,7 @@ app.get('/persona/:personaje', (req, res) => {
 });
 
 const server = app.listen(8001, () => {
-    console.log("Servidor iniciat al port 8001");
+    console.log("Server started at 8001");
 });
 
 let personajeAdivinar = {};
@@ -83,8 +80,7 @@ function obtenerPersonajeAleatorio() {
         const directorioPersonajes = path.join(__dirname, 'persona');
         fs.readdir(directorioPersonajes, (err, archivos) => {
             if (err) {
-                console.log("Error al listar los archivos:", err);
-                reject(err); // Rechazar la promesa si hay un error
+                reject(err); 
                 return;
             }
             const totalArchivos = archivos.length;
@@ -94,16 +90,15 @@ function obtenerPersonajeAleatorio() {
                 const rutaArchivo = path.join(directorioPersonajes, archivoSeleccionado);
                 fs.readFile(rutaArchivo, (err, data) => {
                     if (err) {
-                        console.log("Error al leer el archivo:", err);
-                        reject(err); // Rechazar la promesa si hay un error
+                        reject(err); 
                         return;
                     }
                     const personaje = JSON.parse(data);
                     personajeAdivinar = personaje;
-                    resolve(personajeAdivinar); // Resolver la promesa con el personaje obtenido
+                    resolve(personajeAdivinar);
                 });
             } else {
-                reject(new Error("No hay archivos en el directorio de personajes")); // Rechazar la promesa si no hay archivos
+                reject(new Error("No hay archivos en el directorio de personajes"));
             }
         });
     });
@@ -116,11 +111,8 @@ app.post('/starwardle', bodyParser.json(), (req, res) => {
         .then(personaje => {
             personajesProbados = [];
             res.json({ personajeAdivinar: personajeAdivinar });
-            // Aquí puedes continuar con el resto del código que depende del personaje obtenido
         })
         .catch(error => {
-            console.error("Error al obtener el personaje:", error);
-            // Manejar el error aquí, si es necesario
         });
 
 });
@@ -128,33 +120,28 @@ app.post('/starwardle', bodyParser.json(), (req, res) => {
 
 app.post('/compararPersonaje', bodyParser.json(), (req, res) => {
     let { nombre } = req.body;
-    const nombreConGuionBajo = nombre.replace(/ /g, '_'); // Reemplazar espacios en blanco por guiones bajos
+    const nombreConGuionBajo = nombre.replace(/ /g, '_');  
     if (!personajesProbados.includes(nombre.replace(/ /g, '_'))) {
         personajesProbados.push(nombreConGuionBajo)
 
     }
-    // Si no hay personaje a adivinar, no hay comparación
     if (!personajeAdivinar || !personajeAdivinar.name) {
         return res.status(400).json({ error: "No hay personaje a adivinar" });
     }
 
 
-    // Buscar un archivo con un nombre similar en el directorio
     const directorioPersonajes = path.join(__dirname, 'persona');
     fs.readdir(directorioPersonajes, (err, archivos) => {
         if (err) {
             return res.status(500).json({ error: "Error al listar los archivos" });
         }
 
-        // Verificar si hay un archivo con un nombre similar
         const archivoSimilar = archivos.find(archivo => archivo.toLowerCase() === `${nombreConGuionBajo.toLowerCase()}.json`);
 
         if (!archivoSimilar) {
-            // Si no se encontró un archivo similar, indicar que el personaje no existe
             return res.json({ resultado: "El personaje no existe" });
         }
 
-        // Leer el archivo del personaje similar
         const rutaArchivoSimilar = path.join(directorioPersonajes, archivoSimilar);
         fs.readFile(rutaArchivoSimilar, 'utf8', (err, data) => {
             if (err) {
@@ -163,13 +150,11 @@ app.post('/compararPersonaje', bodyParser.json(), (req, res) => {
 
             const personajeSimilar = JSON.parse(data);
 
-            // Comparar cada atributo del personaje similar con el personaje a adivinar
             const comparaciones = {};
             for (const atributo in personajeAdivinar) {
                 comparaciones[atributo] = personajeSimilar[atributo] === personajeAdivinar[atributo];
             }
 
-            // Enviar el resultado de la comparación y los datos del personaje al cliente
             res.json({ datos: personajeSimilar, comparaciones });
         });
     });
@@ -177,7 +162,7 @@ app.post('/compararPersonaje', bodyParser.json(), (req, res) => {
 
 
 app.post('/buscarPersonajes', bodyParser.json(), (req, res) => {
-    const { name } = req.body; // Asumiendo que el campo de búsqueda se llama 'busqueda'
+    const { name } = req.body; 
     const directorioPersonajes = path.join(__dirname, 'persona');
     fs.readdir(directorioPersonajes, (err, archivos) => {
         if (err) {
@@ -194,21 +179,17 @@ app.post('/buscarPersonajes', bodyParser.json(), (req, res) => {
                 archivosProcesados++;
                 if (!err) {
                     const personaje = JSON.parse(data);
-                    // Aquí ajustas tu lógica de búsqueda según necesites, por ejemplo:
                     if (personaje.name && personaje.name.toLowerCase().includes(name.toLowerCase()) && !personajesProbados.includes(nombreArchivoSinExtension.replace(/ /g, '_'))) {
                         resultadosBusqueda.push(personaje);
                     }
                 }
 
-                // Si ya procesamos todos los archivos, respondemos
                 if (archivosProcesados === archivos.length) {
-                    console.log(resultadosBusqueda)
                     res.json(resultadosBusqueda);
                 }
             });
         });
 
-        // En caso de que no haya archivos
         if (archivos.length === 0) {
             res.json(resultadosBusqueda);
         }
